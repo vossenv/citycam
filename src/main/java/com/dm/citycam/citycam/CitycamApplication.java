2003e5bdeb37ff374b3cd22517bc8ec303ab6a03
jansseneda-adobe <danimaejanssen@gmail.com>
2020-05-30 17:24:07 -0500

    repo

---
+++ src/main/java/com/dm/citycam/citycam/CitycamApplication.java
@@ -0,0 +1,13 @@
+package com.dm.citycam.citycam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitycamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitycamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dm/citycam/citycam/CitycamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dm.citycam.citycam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitycamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
